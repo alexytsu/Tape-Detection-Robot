9516a37ebab0128d7d2e8cd18fbbab05051e89c7
Daniel Huang <Daniel.Huang4739@gmail.com>
2019-05-22 21:35:15 +1000

    added new ipm tool

--- IPM/TestClick.py
+++ IPM/TestClick.py
@@ -6,6 +6,7 @@ pts_src = np.array([[174, 377],[301, 44],[545, 51],[659,379]])
 pts_dst = np.array([[174, 377],[174, 0],[659, 0],[659,379]])
 
 counter = 0
+
 def readmouse(event,x,y,flags,param):
 	global counter
 	if event == cv2.EVENT_LBUTTONDBLCLK:
@@ -15,30 +16,41 @@ def readmouse(event,x,y,flags,param):
 		pts_src[counter][0] = x;
 		pts_src[counter][1] = y;
 		counter+=1
-		
 
-image = cv2.imread("./marsfield.jpg")
+camera = input("input camera: ")
+cam = cv2.VideoCapture(int(camera))
+print("[0] re-tune matrix \n[1] reload old matrix")
 
-cv2.namedWindow('image')
-cv2.setMouseCallback('image',readmouse)
-while(counter < 4):
-	cv2.imshow("image",image)
-	k = cv2.waitKey(20) & 0xFF
-	if k == ord('b'):
-		break
+val = input()
+if(val == 1):
+	cv2.namedWindow('image')
+	cv2.setMouseCallback('image',readmouse)
+	while(counter < 4):
+		ret_val, image = cam.read()
+		cv2.imshow("image",image)
+		k = cv2.waitKey(1) & 0xFF
+		if k == ord('b'):
+			break
+	print(pts_src);
+	x1 = pts_src[1][0] 
+	x2 = pts_src[2][0] 
+	y1 = 0#pts_src[0][1] 
+	y2 = pts_src[2][1]
+	pts_dst = np.array([[x1, y1],[x1, y2],[x2, y2],[x2,y1]])
+	cv2.destroyAllWindows()
+	h, status = cv2.findHomography(pts_src, pts_dst)
+	pickle.dump( h, open( "homographyMatrix.p", "wb" ))
 
-print(pts_src);
-x1 = pts_src[1][0] 
-x2 = pts_src[2][0] 
-y1 = 0#pts_src[0][1] 
-y2 = pts_src[2][1]
 
-pts_dst = np.array([[x1, y1],[x1, y2],[x2, y2],[x2,y1]])
+h = pickle.load( open( "homographyMatrix.p", "rb" ))
+h[0][2] = 0
+h[1][2] = 0
+print(h)
+while True:
+	ret_val, image = cam.read()
+	img = cv2.warpPerspective(image, h, (2*image.shape[1],image.shape[0]))
+	cv2.imshow('my webcam', img)
+	if cv2.waitKey(1) == 27: 
+		break  # esc to quit
 
-
-
-h, status = cv2.findHomography(pts_src, pts_dst)
-pickle.dump( h, open( "homographyMatrix.p", "wb" ))
-im_out = cv2.warpPerspective(image, h, (2*image.shape[1],2*image.shape[0]))
-cv2.imshow("image1",im_out)
-cv2.waitKey(0)
+cv2.destroyAllWindows()

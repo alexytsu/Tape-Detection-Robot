ff862100f8929b7daaaf89bb3921377bd9ab6b80
Alex Su <alexanderytsu@gmail.com>
2019-05-22 18:13:27 +1000

    blacked

--- tester.py
+++ tester.py
@@ -17,6 +17,7 @@ DEBUG = True
 CAMERA = True
 SER = None
 
+
 def mask_image(image, model, frame_n):
 
     if DEBUG:
@@ -24,18 +25,19 @@ def mask_image(image, model, frame_n):
 
     # Convert BGR to HSV
     hsvImage = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    H = hsvImage[:,:,0]
-    S = hsvImage[:,:,1]
+    H = hsvImage[:, :, 0]
+    S = hsvImage[:, :, 1]
 
     # find how long the one dimensional form needs to be
     one_dim_length = hsvImage.shape[0] * hsvImage.shape[1]
     # now spread out the image [HSV]
     Xnew = np.reshape(hsvImage, (one_dim_length, 3))
     # only take H and S
-    Xnew = Xnew[:,0:2]
+    Xnew = Xnew[:, 0:2]
 
     return model.predict(Xnew)
-    
+
+
 def show_masks(ynew, image):
     roi = np.where(ynew == 1)[0]
     for loc in roi:
@@ -49,18 +51,17 @@ def show_masks(ynew, image):
         col = loc % image.shape[1]
         image[row][col] = (200, 200, 100)
 
-
     roi = np.where(ynew == 4)[0]
     for loc in roi:
         row = int(loc / image.shape[1])
         col = loc % image.shape[1]
         image[row][col] = (0, 0, 0)
 
-
     image = cv2.resize(image, (1280, 720))
-    cv2.imshow('res', image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow("res", image)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         exit()
+
 
 def plan_steering(classified, image):
     height = image.shape[0]
@@ -77,13 +78,13 @@ def plan_steering(classified, image):
     for point in pointList:
         try:
             x, y = point
-            lateral_error += x - width/2 
+            lateral_error += x - width / 2
             cv2.circle(image, (x, y), 2, (0, 100, 0), 1)
         except:
             pass
 
     try:
-        lateral_error = lateral_error/len(pointList)
+        lateral_error = lateral_error / len(pointList)
     except:
         print("No Points")
 
@@ -91,7 +92,7 @@ def plan_steering(classified, image):
     for point in pointList:
         x, y = point
         try:
-            cv2.circle(image, (x,y), 2, (255,100,100), 1)
+            cv2.circle(image, (x, y), 2, (255, 100, 100), 1)
         except:
             print("Failed")
 
@@ -99,7 +100,7 @@ def plan_steering(classified, image):
     for point in pointList:
         x, y = point
         try:
-            cv2.circle(image, (x,y), 2, (0,100,100), 1)
+            cv2.circle(image, (x, y), 2, (0, 100, 100), 1)
         except:
             print("Failed")
 
@@ -110,31 +111,31 @@ def plan_steering(classified, image):
     lineList = cFrame.getBlueLine()
     for line in lineList:
         end, start = line
-        x1, y1 = end;
-        x2, y2 = start;
-        cv2.line(image, (x1,y1), (x2,y2), (255,0,0), 1)
+        x1, y1 = end
+        x2, y2 = start
+        cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 1)
         blueLine = line
 
     lineList = cFrame.getYellowLine()
     for line in lineList:
         end, start = line
-        x1, y1 = end;
-        x2, y2 = start;
-        cv2.line(image, (x1,y1), (x2,y2), (0,255,255), 1)
+        x1, y1 = end
+        x2, y2 = start
+        cv2.line(image, (x1, y1), (x2, y2), (0, 255, 255), 1)
         yellowLine = line
 
     lineList = cFrame.getNavLine()
     for line in lineList:
         end, start = line
-        x1, y1 = start;
-        x2, y2 = end;
-        cv2.line(image, (x1,y1), (x2,y2), (0,255,0), 2)
+        x1, y1 = start
+        x2, y2 = end
+        cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
         navLine = line
 
     def getLineAttributes(line):
         end, start = line
-        x1, y1 = start;
-        x2, y2 = end;
+        x1, y1 = start
+        x2, y2 = end
         angle_hor = x2 - x1
         angle_ver = y1 - y2
         angle_error = math.degrees(math.atan2(angle_hor, angle_ver))
@@ -142,11 +143,38 @@ def plan_steering(classified, image):
         P = 0.2
         steering_angle = I * lateral_error + P * angle_error
         return lateral_error, angle_error, steering_angle
-    
+
     def writeLineAttributes(lat, angle, steer, image):
-        cv2.putText(image, f'Lateral Error: {lat:.2f}', (10, 15), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,0), 1, cv2.LINE_AA)
-        cv2.putText(image, f'Angular Error: {angle:.2f}', (10, 30), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,0), 1, cv2.LINE_AA)
-        cv2.putText(image, f'Steering Angle: {steer:.2f}', (10, 45), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,0), 1, cv2.LINE_AA)
+        cv2.putText(
+            image,
+            f"Lateral Error: {lat:.2f}",
+            (10, 15),
+            cv2.FONT_HERSHEY_PLAIN,
+            1,
+            (255, 255, 0),
+            1,
+            cv2.LINE_AA,
+        )
+        cv2.putText(
+            image,
+            f"Angular Error: {angle:.2f}",
+            (10, 30),
+            cv2.FONT_HERSHEY_PLAIN,
+            1,
+            (255, 255, 0),
+            1,
+            cv2.LINE_AA,
+        )
+        cv2.putText(
+            image,
+            f"Steering Angle: {steer:.2f}",
+            (10, 45),
+            cv2.FONT_HERSHEY_PLAIN,
+            1,
+            (255, 255, 0),
+            1,
+            cv2.LINE_AA,
+        )
 
     retval = 0
 
@@ -176,20 +204,20 @@ def plan_steering(classified, image):
         writeLineAttributes(lat, angle, steer, image)
         retval = angle
 
-    cv2.imshow('res', cv2.resize(image, (1280, 720)))
-    if(CAMERA):
+    cv2.imshow("res", cv2.resize(image, (1280, 720)))
+    if CAMERA:
         cv2.waitKey(1)
     else:
-        if cv2.waitKey(0) & 0xFF == ord('q'):
+        if cv2.waitKey(0) & 0xFF == ord("q"):
             exit()
     return retval
 
 
 def applyIPT(image):
-    pts_src = np.array([[174, 377],[301, 44],[545, 51],[659,379]])
-    pts_dst = np.array([[174, 377],[174, 0],[659, 0],[659,379]])
+    pts_src = np.array([[174, 377], [301, 44], [545, 51], [659, 379]])
+    pts_dst = np.array([[174, 377], [174, 0], [659, 0], [659, 379]])
     h, status = cv2.findHomography(pts_src, pts_dst)
-    im_out  = cv2.warpPerspective(image, h, (image.shape[1],image.shape[0]))
+    im_out = cv2.warpPerspective(image, h, (image.shape[1], image.shape[0]))
 
     return im_out
 
@@ -230,7 +258,7 @@ if __name__ == "__main__":
         print("Running with CAMERA mode on by default")
         print("Use 'python tester.py --test' to use a saved video")
     else:
-        if sys.argv[1] == '--test':
+        if sys.argv[1] == "--test":
             CAMERA = False
     try:
         SER = getSerialPort()

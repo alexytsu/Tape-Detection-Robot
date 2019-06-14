36787ce7b6dbea9e23bd4bb3cb1bb47d640366ad
Daniel <Daniel.Huang4739@gmail.com>
2019-06-14 21:20:50 +1000

    arduino fix

--- arduino.py
+++ arduino.py
@@ -5,21 +5,14 @@ import time
 #written by Daniel Huang 
 
 def SendSpeed(ser, steering, speed): #servo and motor interface
-    adjusted_angle = 85
-    if abs(steering) < 3:
-        adjusted_angle = 85 - steering
-    else:
-        if steering > 3:
-            adjusted_angle = 60
-        elif steering < -3:
-            adjusted_angle = 115
+    adjusted_angle = 90 + steering
     checksum = (70+36+adjusted_angle+speed)%256 #calc 8bit checksum 
     try:
         ser.write((('F${}{}{}').format(chr(speed),chr(adjusted_angle),chr(checksum))).encode('utf-8')) #format into ascii string and then write to port
     except:
         pass
     
-#max steering range is 115 to 60
+#max steering range is 125 to 55
 
 def getSerialPort():
     ser = serial.Serial('/dev/ttyUSB0') #open serial port 
@@ -38,8 +31,8 @@ if __name__ == "__main__":
         time.sleep(0.1);
         steering+=1
         speed +=1
-        if steering > 25:
-            steering = -25
+        if steering > 35:
+            steering = -35
         if speed > 95:
             speed = 90;
         print ("angle: ", steering)
